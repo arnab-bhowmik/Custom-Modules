0b2974bf15a31a031f7e191be2107ed992a822af
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-02-08 19:30:44 +0530

    Re-introduced User credentials for authentication to RabbitMQ service

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -11,16 +11,18 @@ export abstract class Publisher<T extends Event> {
   private connection: amqp.Connection | null = null;
   private channel: amqp.Channel | null = null;
 
-  constructor(public exchange: string, public key: string, public rabbitmq_k8s_service: string, public rabbitmq_k8s_service_port: number) {
+  constructor(public exchange: string, public key: string, public rabbitmq_username: string, public rabbitmq_password: string, public rabbitmq_k8s_service: string, public rabbitmq_k8s_service_port: number) {
     // Initialize properties  
     this.exchange                   = exchange;
     this.key                        = key;
+    this.rabbitmq_username          = rabbitmq_username;
+    this.rabbitmq_password          = rabbitmq_password;
     this.rabbitmq_k8s_service       = rabbitmq_k8s_service;
     this.rabbitmq_k8s_service_port  = rabbitmq_k8s_service_port;
   }
 
   async createChannel(): Promise<void> {
-    const rabbitmqUrl = `amqp://${this.rabbitmq_k8s_service}:${this.rabbitmq_k8s_service_port}`;
+    const rabbitmqUrl = `amqp://${this.rabbitmq_username}:${this.rabbitmq_password}@${this.rabbitmq_k8s_service}:${this.rabbitmq_k8s_service_port}`;
     this.connection = await amqp.connect(rabbitmqUrl);
     this.channel = await this.connection.createChannel();
     await this.channel.assertExchange(this.exchange, 'topic', { durable: false });

83b81fe109f7f7335348868c4becdb779b9f7a5c
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-03-06 13:32:26 +0530

    Updated NotFound Error definition to have a custom message for better clarity

--- src/errors/not-found-errors.ts
+++ src/errors/not-found-errors.ts
@@ -3,14 +3,14 @@ import { CustomError } from "./custom-errors";
 export class NotFoundError extends CustomError {
     statusCode = 404;
 
-    constructor() {
-        super('Route Not Found');
+    constructor(public message: string) {
+        super(message);
 
         //We are extending a built-in class
         Object.setPrototypeOf(this, NotFoundError.prototype); 
     }
 
     serializeErrors() {
-        return [{ message: 'Not Found' }];
+        return [{ message: this.message }];
     }
 }

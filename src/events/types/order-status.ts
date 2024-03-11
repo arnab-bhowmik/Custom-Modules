2f412e0a9213b021a6affbe913c84125e4d80672
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-03-11 10:48:51 +0530

    Updated Order status values from lowercase to PascalCase

--- src/events/types/order-status.ts
+++ src/events/types/order-status.ts
@@ -1,5 +1,5 @@
 export enum OrderStatus {
-    Created   = 'created',     // When Order is created and the corresponding ticket has been successfully reserved. But awaiting payment
-    Cancelled = 'cancelled',   // When Order has been cancelled by the User or the Payment deadline has been exceeded
-    Completed = 'completed'    // When Order is created and the corresponding ticket has been successfully reserved & payment has been completed
+    Created   = 'Created',     // When Order is created and the corresponding ticket has been successfully reserved. But awaiting payment
+    Cancelled = 'Cancelled',   // When Order has been cancelled by the User or the Payment deadline has been exceeded
+    Completed = 'Completed'    // When Order is created and the corresponding ticket has been successfully reserved & payment has been completed
 }
